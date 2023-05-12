3e791bc549eff1c0004df4da691ce818274b7c93
Angelo Pareto <angelopareto98@gmail.com>
2023-05-12 23:27:14 +0300

    add card in PageView

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallet_app_ui/util/my_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,68 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: const [
+                    Text(
+                      "My",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      " Cards",
+                      style: TextStyle(fontSize: 26),
+                    ),
+                  ],
+                ),
+                Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[400], shape: BoxShape.circle),
+                    child: const Icon(Icons.add))
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 200,
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                MyCard(
+                  balance: 5250.20,
+                  cardNumber: 123456789,
+                  expiryMonth: 12,
+                  expiryYear: 24,
+                  color: Colors.deepPurple[300],
+                ),
+                MyCard(
+                  balance: 250.25,
+                  cardNumber: 987654321,
+                  expiryMonth: 08,
+                  expiryYear: 25,
+                  color: Colors.green[300],
+                ),
+                MyCard(
+                  balance: 300.20,
+                  cardNumber: 456789123,
+                  expiryMonth: 11,
+                  expiryYear: 23,
+                  color: Colors.yellow[600],
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
---
+++ lib/util/my_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  final double balance;
+  final int cardNumber;
+  final int expiryMonth;
+  final int expiryYear;
+  final color;
+
+  const MyCard(
+      {super.key,
+      required this.balance,
+      required this.cardNumber,
+      required this.expiryMonth,
+      required this.expiryYear,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Container(
+        width: 300,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Balance",
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "\$${balance}",
+              style: TextStyle(color: Colors.white, fontSize: 26),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  cardNumber.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "${expiryMonth}/${expiryYear}",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
